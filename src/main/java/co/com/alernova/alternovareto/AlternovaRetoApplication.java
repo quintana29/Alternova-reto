328ad73988b73cc8f0ce089954e01997da8b2f56
quintana29 <jhonatanquintana29@gmail.com>
2023-06-03 11:46:38 -0500

    Creando proyecto

---
+++ src/main/java/co/com/alernova/alternovareto/AlternovaRetoApplication.java
@@ -0,0 +1,13 @@
+package co.com.alernova.alternovareto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternovaRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlternovaRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/alernova/alternovareto/AlternovaRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.alernova.alternovareto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlternovaRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
